cbc672fa2591b77e4a5d9ad00af2152f46fb6c7e
mrinex <mrinex@mail.ru>
2019-11-02 19:17:09 +0300

    feat: add button illumination

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let firstrow = ['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'];
 let secondrow = ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\'];
-let third = ['Caps Lock','a','s','d','f','g','h','j','k','l',';',"'",'Enter'];
-let fourth = ['Shift','z','x','c','v','b','n','m',',','.','/','Shift','up'];
-let fifth = ['Ctrl','Win','Alt','space','Alt','Ctrl','left','dn','right'];
+let third = ['CapsLock','a','s','d','f','g','h','j','k','l',';',"'",'Enter'];
+let fourth = ['Shift','z','x','c','v','b','n','m',',','.','/','up','Shift'];
+let fifth = ['Ctrl','Win','Alt'," ",'Alt','left','dn','right','Ctrl'];
 
 //draw input
 let inp = document.createElement("textarea");
@@ -19,9 +19,8 @@ main.append(div);
 
 for(let i = 0; i < firstrow.length; i++){
     let divnum = document.createElement("div");
-    if(firstrow[i]==='Backspace')
-        divnum.id = 'backspace';
     divnum.className = "num";
+    divnum.id = `${firstrow[i]}`;
     divnum.innerHTML = firstrow[i];
     div.append(divnum);
 }
@@ -31,11 +30,8 @@ divv.id = 'row';
 main.append(divv);
 for(let i = 0; i < secondrow.length; i++){
     let divnum = document.createElement("div");
-    if(secondrow[i]==='Tab')
-        divnum.id = 'tab';
-    if(secondrow[i]==='\\')
-        divnum.id = 'll';
     divnum.className = "num";
+    divnum.id = `${secondrow[i]}`;
     divnum.innerHTML = secondrow[i];
     divv.append(divnum);
 }
@@ -45,11 +41,8 @@ divvv.id = 'row';
 main.append(divvv);
 for(let i = 0; i < third.length; i++){
     let divnum = document.createElement("div");
-    if(third[i]==='Caps Lock')
-        divnum.id = 'capslock';
-    if(third[i]==='Enter')
-        divnum.id = 'enter';
     divnum.className= "num";
+    divnum.id = `${third[i]}`;
     divnum.innerHTML = third[i];
     divvv.append(divnum);
 }
@@ -59,9 +52,8 @@ divvvv.id = 'row';
 main.append(divvvv);
 for(let i = 0; i < fourth.length; i++){
     let divnum = document.createElement("div");
-    if(fourth[i]==='Shift')
-        divnum.id = 'shift';
     divnum.className = "num";
+    divnum.id = `${fourth[i]}`;
     divnum.innerHTML = fourth[i];
     divvvv.append(divnum);
 }
@@ -71,21 +63,19 @@ divvvvv.id = 'row';
 main.append(divvvvv);
 for(let i = 0; i < fifth.length; i++){
     let divnum = document.createElement("div");
-    if(fifth[i]==='space')
-        divnum.id = 'space';
-    if(fifth[i]==='Ctrl')
-        divnum.id = 'ctrl';
     divnum.className = "num";
+    divnum.id = `${fifth[i]}`;
     divnum.innerHTML = fifth[i];
     divvvvv.append(divnum);
 }
 
 
 window.addEventListener("keydown", function(event) {
-    let str = 'event.key: [' + event.key + '] event.code: [' + event.code + ']';
-
-    //draw input
-    let inp = document.getElementById("inp");
-    inp.innerHTML = str;
-    //document.body.appendChild(inp); 
+    //let str = 'event.key: [' + event.key + '] event.code: [' + event.code + ']';
+    let inp = this.document.getElementById(`${event.key}`);
+    inp.style.background = "grey";
+  }, true);
+  window.addEventListener("keyup", function(event) {
+    let inp = this.document.getElementById(`${event.key}`);
+    inp.style.background = null;
   }, true);
